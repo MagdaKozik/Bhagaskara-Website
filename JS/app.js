0e06934f51616c92dd711c9c6e76fb3f5411fdd8
Magda <kozik.magda@gmail.com>
2015-12-18 16:58:47 +0100

    Added slider to Our Team section

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+function stickyMenu (){
   var menu = $("#sticky-menu");
   var menuOfSetFromTop = menu.position().top;
   
@@ -9,14 +11,58 @@ $(document).ready(function(){
     else {
       menu.removeClass("sticky");
     }
+   });
+
+
+function smoothScroll() {
+    $('a').click(function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 2000);
+        return false;
+    });
+  }
+}
+
+function slider(){
+
+  var person = $(".person");
+  var personLength = person.length;
+  var counter = 1;
+
+  $(".prev").click(function(){
+
+    var activeMember = $(".active");
+    var actualNumber = activeMember.index();
+
+    person.removeClass("active");
+
+    if (actualNumber <= 0){
+      counter =personLength-1;
+    } else {
+      counter--;
+    }
+    person.eq(counter).addClass("active");
 
   });
 
-// $("#button").click(function() {
-//     $('html, body').animate({
-//         scrollTop: $("#elementtoScrollToID").offset().top
-//     }, 2000);
-// }); 
+  $(".next").click(function(){
+    var activeMember = $(".active");
+    var actualNumber = activeMember.index();
 
+    person.removeClass("active");
+    
+      if (actualNumber >= personLength -1 ){
+          counter = 0;
+      } else {
+        counter++;
+      }
+
+      person.eq(counter).addClass("active");
+
+  });
+}
+
+slider();
 
 });
